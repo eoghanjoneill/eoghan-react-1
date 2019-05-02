07ab7d96fc7e7dc619849cf2b9bd43345cee7478
eoghanjoneill <eoghanjoneill@gmail.com>
2019-05-02 16:37:46 +0100

    game win check mechanism

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,37 +1,64 @@
 import React from 'react';
 import './Game.css';
 
-  class Square extends React.Component {
-    constructor(props) {
-      super(props);
-        this.state = {
-          value: false
-        };
-    }
-    render() {
-      return (
-        <button className="square" onClick={() => this.setState({value: !this.state.value})}>
-          {this.state.value ? "O" : "X"}
-        </button>
-      );
-    }
+  function Square (props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+    
   }
   
   class Board extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {vals: []}
-    }
+      this.state = {
+        squares: Array(9).fill(null),
+        next: "X",
+        winner: null
+      };
+      this.winners = [[0,1,2],[3,4,5],[6,7,8],
+                       [0,3,6],[1,4,7],[2,5,8],
+                       [0,4,8],[2,4,6]];
+    }    
+
     renderSquare(i) {
-      return <Square value={i}/>;
+      return (
+          <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+          />
+        );
     }
   
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      const lastMover = this.state.next;
+      squares[i] = lastMover;
+      const next = lastMover === "X" ? "O" : "X";
+      let winner = this.state.winner;
+      if (this.checkForWin(squares, lastMover)) {
+        winner = lastMover;
+      }
+      this.setState({squares: squares, next: next, winner: winner});
+    }
+
+    checkForWin(squares, lastMover) {
+      //only need to check for winning combinations for the last mover, i.e. if X was last to move then O cannot win on this turn      
+      return this.winners.some((indices) => {
+        return indices.every((squarePos) => squares[squarePos] === lastMover)
+      });      
+    }
+    
     render() {
-      const status = 'Next player: X';
+      const status = `Next player: ${this.state.next}`;
+      const winStatus = this.state.winner ? `Hot dog, we have a wiener: ${this.state.winner}!` : '';
   
       return (
         <div>
           <div className="status">{status}</div>
+          <div className="status">{winStatus}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
